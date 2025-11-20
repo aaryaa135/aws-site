d0606edc0587abdbc344c024f4fdb99dd512de6e
Aarya Gupta <tg1570@gmail.com>
2025-11-20 11:40:50 +0530

    Change userPoolClientId in config.js
    Updated userPoolClientId in the configuration.

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-north-1_0hc7OR2Ix', 
-        userPoolClientId: '6p1vljc822r3qggf7c4cc6p8vv', 
+        userPoolClientId: '7622see0h1n6ipgb6o3iojt14k', 
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://seachek56l.execute-api.eu-north-1.amazonaws.com/dev' ,
     }
 };
+
 
 
